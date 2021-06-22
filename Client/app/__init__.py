56adbcf4205abb1fcb2fbd1b16dc3128935de05c
21600212 <21600212@handong.edu>
2021-06-22 16:38:13 +0900

    Start to serve Flask Client

---
+++ Client/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return render_template('index.html')
+
+@app.route('/result')
+def result():
+   return render_template('result.html')
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=5000)
