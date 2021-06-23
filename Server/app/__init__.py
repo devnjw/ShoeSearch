03568b6d3edd2dca563e5c15bedcdca5c31bf7d6
21600212 <21600212@handong.edu>
2021-06-23 14:12:18 +0900

    Connecting Server and Client

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 from .config import BaseConfig
 
@@ -10,6 +11,7 @@ migrate = Migrate(compare_type=True)
 
 def create_app():
     app = Flask(__name__)
+
     app.config.from_object(BaseConfig)
     api = Api(app)
 
@@ -20,7 +22,9 @@ def create_app():
     from . import models
 
     from .crawler import Crawl
-
     api.add_resource(Crawl, '/crawl')
+
+    from .item_controller import ItemList
+    api.add_resource(ItemList, '/items')
 
     return app
