f22f9e9d9c048080112213bcf6c367b9193c7d0e
21600212 <21600212@handong.edu>
2021-07-02 17:36:16 +0900

    No image file Exception Handling

--- Client/app/__init__.py
+++ Client/app/__init__.py
@@ -1,17 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 import json
 import requests
+from .config import BaseConfig
 
 app = Flask(__name__)
+app.config.from_object(BaseConfig)
 
 @app.route('/')
-def hello_world():
+def home():
    url = 'http://127.0.0.1:5050/home'
    requests.get(url)
 
    return render_template('index.html')
 
-@app.route('/result')
+@app.route('/search')
 def string_search():
    keyword = request.args.get('keyword')
    if not keyword:
@@ -26,11 +28,16 @@ def string_search():
 def image_search():
    file = request.files['file']
 
-   url = "http://127.0.0.1:5050/search/image"
-   files = {'file': file}
-   res = requests.post(url, files=files).json()
+   if not file:
+      flash("이미지 검색을 사용하려면, 이미지를 넣어주세요.\\nPlease put the image in for image search.")
+      return render_template('index.html')
 
-   return render_template('result.html', items=res['data'])
+   else:
+      url = "http://127.0.0.1:5050/search/image"
+      files = {'file': file}
+      res = requests.post(url, files=files).json()
+
+      return render_template('result.html', items=res['data'])
 
 if __name__ == "__main__":
    app.run(debug=True, host='0.0.0.0', port=9000)
