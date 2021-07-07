a2d18eb159193a592dead6905e5f9499cf8e51ba
21600212 <21600212@handong.edu>
2021-07-07 16:31:53 +0900

    edit shoogle image button

--- Client/app/__init__.py
+++ Client/app/__init__.py
@@ -40,4 +40,4 @@ def image_search():
       return render_template('result.html', items=res['data'])
 
 if __name__ == "__main__":
-   app.run(debug=True, host='0.0.0.0', port=9000)
+   app.run(debug=True, host='0.0.0.0', port=5000)
