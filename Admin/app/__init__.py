83269e550b9ca62843b42c231839d63b1d12eb62
21600212 <21600212@handong.edu>
2021-10-28 20:07:22 +0900

    refactoring admin web server

---
+++ Admin/app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+import json
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['get'])
+def image_search():
+    url = "http://127.0.0.1:5050/admin/image"
+    res = requests.get(url).json()
+
+    return render_template('result.html', items=res['data'])
+
+if __name__ == "__main__":
+   app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ Admin/app/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(find_dotenv())
+
+class BaseConfig(object):
+    DEBUG = True
+    use_reloader=True
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "my super secret key"
